a76f8d1b9eba9c4842d45a1e91cc0ab5f0a41b80
nik afz <nikafz980@gmail.com>
2024-03-02 01:08:47 +0330

    feat:  added custom text

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ function App() {
 
   return (
     <>
-      {/* <Time/> */}
-      {/* <Contact/> */}
+      <Time/>
+      <Contact/>
       <SearchBlockText/>
       
     </>
---
+++ src/components/CustomText.jsx
@@ -0,0 +1,11 @@
+function CustomText(props) {
+  return (
+    <>
+      <div className="custom-text--container">
+        <p className="custom-text">consectetur adipisicing elit Facilis, alias</p>
+      </div>
+    </>
+  )
+};
+
+export default CustomText;
