06381d3ad70b3c735e1ebd7674dafb99742d57db
NikAfz <nikafz980@gmail.com>
2024-03-22 15:15:43 +0330

    feat : working search bar

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,13 +1,33 @@
+import React, { useEffect, useState } from "react";
+
 function Time(props) {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 20000);
+
+    
+    return () => clearInterval(interval);
+  }, []);
+
+
+  const hours = time.getHours();
+  const minutes = ('0' + time.getMinutes()).slice(-2);
+  const day = time.toLocaleDateString('en-EN', { weekday: 'long' });
+  const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const month = monthNames[time.getMonth()];
+  const fullYear = time.getDate() + "/" + (time.getMonth() + 1) + "/" + time.getFullYear();
+
   return (
-    <>
-      <div class="time--container">
-        <h1 id="hours" class="time">loading</h1>
-        <h3 id="dayMonth" class="time--date">loading</h3>
-        <h3 id="fullYear" class="time--date">loading</h3>
-      </div>
-    </>
-  )
-};
+    <div className="time--container">
+      <h1 className="time">{hours}:{minutes}</h1>
+      <h3 className="time--date">{day}/{month}</h3>
+      <h3 className="time--date">{fullYear}</h3>
+    </div>
+  );
+}
 
 export default Time;
+
