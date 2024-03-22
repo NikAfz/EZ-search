965bd9fc6ad6c65c619c8fddde61bcec992b24f7
NikAfz <nikafz980@gmail.com>
2024-03-22 15:14:57 +0330

    fix : stylings

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,9 +7,9 @@ function Contact(props) {
     <>
       <div class="dev--media-container">
       <h2>Contact Dev:</h2>
-      <a href="mailto:nikafz980@gmail.com" class="dev--media-link"> <AlternateEmailIcon/> Email</a>
-      <a href="https://github.com/NikAfz" class="dev--media-link"> <GitHubIcon/> GitHub</a>
-      <a href="https://www.linkedin.com/in/nik-afz-4a0618268/" class="dev--media-link"> <LinkedInIcon/> LinkedIn</a>
+      <a href="mailto:nikafz980@gmail.com" className="dev--media-link"> <AlternateEmailIcon/> Email</a>
+      <a href="https://github.com/NikAfz" className="dev--media-link"> <GitHubIcon/> GitHub</a>
+      <a href="https://www.linkedin.com/in/nik-afz-4a0618268/" className="dev--media-link"> <LinkedInIcon/> LinkedIn</a>
     </div>
     </>
   )
