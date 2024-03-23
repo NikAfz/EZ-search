41c1464a3c980a5f45dd9e872d0dc4c660187742
NikAfz <nikafz980@gmail.com>
2024-03-23 12:43:47 +0330

    feat : search history

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,31 @@
+import { useState } from "react";
+
 function Search(props) {
+
+  const [history,setHistory] = useState(["hi",'why',"die","five","night"])
+  const [isfocus,setIsfocus] = useState(false)
+
   return (
-    <form class="form" action="https://www.google.com/search?q=" method="get" autoComplete="off" >
-      <input  name="q" type="search" placeholder="Search" className="input--filed"/>
+    <>
+    <form className="form" action="https://www.google.com/search?q=" method="get" autoComplete="off" >
+      <input  name="q" type="search" placeholder="Search" className="input--filed" onBlur={() => setIsfocus(false)} onFocus={() => setIsfocus(true)} />
+      {isfocus ? <div className="history--container">
+        <div className="history">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, nisi!
+        </div>
+        <div className="history">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, vitae!
+        </div>
+        <div className="history">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, laborum!
+        </div>
+        <div className="history">
+          Lorem ipsum dolor sit amet consectetur adipisicingamet consectetur adipisicingamet consectetur adipisicingamet consectetur adipisicing elit. Dolore, natus quas! Reprehenderit quae consectetur maiores.
+        </div>
+      </div>: null}
     </form>
+    
+    </>
   )
 };
 
