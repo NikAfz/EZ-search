42a219d47676befe0c4b77997520a755b067cfde
NikAfz <nikafz980@gmail.com>
2024-09-28 14:23:39 +0330

    fix : curser

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,51 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './index.css'
-import Time from './components/Time'
-import Contact from './components/Contact'
-import SearchBlockText from './components/SearchBlockText'
-import SidePanel from './components/SidePanel'
-import ShortCutInput from './components/ShortCutInput'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./index.css";
+import Time from "./components/Time";
+import Contact from "./components/Contact";
+import SearchBlockText from "./components/SearchBlockText";
+import SidePanel from "./components/SidePanel";
+import ShortCutInput from "./components/ShortCutInput";
 
 function App() {
-  const [showTime, setShowTime] = useState(true)
-  const [showContactDev, setShowContactDev] = useState(true)
-  const [showSidePanel, setShowSidePanel] = useState(true)
-  const [showCustomText, setShowCustomText] = useState(true)
-  const [showShortCutInp, setShowShortCutInp] = useState(false)
+  const [showTime, setShowTime] = useState(true);
+  const [showContactDev, setShowContactDev] = useState(true);
+  const [showSidePanel, setShowSidePanel] = useState(true);
+  const [showCustomText, setShowCustomText] = useState(true);
+  const [showShortCutInp, setShowShortCutInp] = useState(false);
 
-  const [customText, setCustomText] = useState(JSON.parse(localStorage.getItem("textKey")) || "you can change some stuff in the settings")
+  const [customText, setCustomText] = useState(
+    JSON.parse(localStorage.getItem("textKey")) ||
+      "you can change some stuff in the settings"
+  );
 
-  const [backgroundColor, setBackgroundColor] = useState(JSON.parse(localStorage.getItem('backgroundColor')) || "p")
- 
-  const [shortCutPlace, setShortCutPlace] = useState('');
+  const [backgroundColor, setBackgroundColor] = useState(
+    JSON.parse(localStorage.getItem("backgroundColor")) || "p"
+  );
+
+  const [shortCutPlace, setShortCutPlace] = useState("");
 
   let [data, setData] = useState([
     {
-      "place": 'r',
-      "name": 'Right',
-      "URL": 'https://github.com/NikAfz/cryptopia/blob/main/src/CoinList.jsx',
-      "img": 's'
+      id: 0,
+      place: "m",
+      name: "NIkAfz",
+      URL: "https://nikafz.github.io/portfolio/",
+      img: "",
     },
   ]);
 
   useEffect(() => {
+    const shortCutHistory = localStorage.getItem("shortCutKey");
     const timeHistory = localStorage.getItem("timeSettingKey");
     const contactHistory = localStorage.getItem("contactSettingKey");
     const panelHistory = localStorage.getItem("panelSettingKey");
     const textHistory1 = localStorage.getItem("textSettingKey");
 
+    if (shortCutHistory) {
+      setData(JSON.parse(shortCutHistory));
+    }
     if (timeHistory) {
       setShowTime(JSON.parse(timeHistory));
     }
@@ -52,37 +62,45 @@ function App() {
 
   return (
     <>
-      {showShortCutInp? <ShortCutInput
-        setShowShortCutInp={setShowShortCutInp}
-        shortCutPlace={shortCutPlace}
-        setData={setData}
-        data={data}
-      /> : null}
-      
-      {showTime? <Time /> : null}
-      {showContactDev? <Contact/> : null}
+      {showShortCutInp ? (
+        <ShortCutInput
+          setShowShortCutInp={setShowShortCutInp}
+          shortCutPlace={shortCutPlace}
+          setData={setData}
+          data={data}
+        />
+      ) : null}
 
-      <SearchBlockText 
-        showSidePanel={showSidePanel} 
+      {showTime ? <Time /> : null}
+      {showContactDev ? <Contact /> : null}
+
+      <SearchBlockText
+        showSidePanel={showSidePanel}
         showCustomText={showCustomText}
         customText={customText}
         backgroundColor={backgroundColor}
         setShortCutPlace={setShortCutPlace}
         setShowShortCutInp={setShowShortCutInp}
         data={data}
+        setData={setData}
       />
-      
-      <SidePanel 
-        showTime={showTime} setShowTime={setShowTime}
-        showContactDev={showContactDev} setShowContactDev={setShowContactDev}
-        showSidePanel={showSidePanel} setShowSidePanel={setShowSidePanel}
-        showCustomText={showCustomText} setShowCustomText={setShowCustomText}
-        customText={customText} setCustomText={setCustomText}
-        backgroundColor={backgroundColor} setBackgroundColor={setBackgroundColor}
+
+      <SidePanel
+        showTime={showTime}
+        setShowTime={setShowTime}
+        showContactDev={showContactDev}
+        setShowContactDev={setShowContactDev}
+        showSidePanel={showSidePanel}
+        setShowSidePanel={setShowSidePanel}
+        showCustomText={showCustomText}
+        setShowCustomText={setShowCustomText}
+        customText={customText}
+        setCustomText={setCustomText}
+        backgroundColor={backgroundColor}
+        setBackgroundColor={setBackgroundColor}
       />
-    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
